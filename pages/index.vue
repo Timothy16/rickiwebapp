d074d59ffc46b42d5765c8da9c77f092a91262dc
Afolabi Timothy <afolabi.timothy51@gmail.com>
2023-05-12 11:42:49 +0100

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
     <div>
+        <Head>
+            <Title>Ricki &mdash; Trade your Giftcards for Instant Cash at the Speed of Light.</Title>
+            <Meta name="description"  content='Today, Ricki is the world’s leading gift card trading platform. We are making a promise to be legit, efficient and provide quality service. Our Mission is to be the household name for giftcard exchange in Nigeria.' />
+        </Head>
         <Hero />
         <Advert />
         <GetPaid />
@@ -12,16 +16,16 @@
 <script setup>
 
     useHead({
-        title: 'Ricki',
-        meta: [
-        { hid: 'description', name : 'description', content : 'Today, Ricki is the world’s leading gift card trading platform. We are making a promise to be legit, efficient and provide quality service. Our Mission is to be the household name for giftcard exchange in Nigeria.'},
-                {property : 'og:url', content : 'Rickiapp.com'},
-                {property : 'og:url', content : 'Rickiapp.com'},
-                {name : 'twitter:card' , content : 'summary_large_image'}
-        ],
-        bodyAttrs: {
-          class: 'test'
-        }
+        // title: 'Ricki',
+        // meta: [
+        // { hid: 'description', name : 'description', content : 'Today, Ricki is the world’s leading gift card trading platform. We are making a promise to be legit, efficient and provide quality service. Our Mission is to be the household name for giftcard exchange in Nigeria.'},
+        //         {property : 'og:url', content : 'Rickiapp.com'},
+        //         {property : 'og:url', content : 'Rickiapp.com'},
+        //         {name : 'twitter:card' , content : 'summary_large_image'}
+        // ],
+        // bodyAttrs: {
+        //   class: 'test'
+        // }
     })
 </script>
 
